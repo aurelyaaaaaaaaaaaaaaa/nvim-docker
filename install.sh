af71f6bf5da8896e76423d26b275900e1b74cc0e
aurelya <61779349+aurelyaaaaaaaaaaaaaaa@users.noreply.github.com>
2025-02-26 14:12:17 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ rm -rf "${INSTALL_DIR}.config"
 
 # NEOVIM
 wget https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-x86_64.tar.gz
-tar xzvf nvim-linux64.tar.gz
+tar xzvf nvim-linux-x86_64.tar.gz
  ln -s "${INSTALL_DIR}nvim-linux64/bin/nvim" /usr/local/bin/nvim
  apt-get install -y python3-venv
 
